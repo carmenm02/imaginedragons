9a1f540da1f6263220d4c268e2733a162b1d713e
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-06-01 12:07:04 +0200

    he intentado arreglar un fallo de esta funcion pero no me detecta la lista timeline :(

--- twitter.py
+++ twitter.py
@@ -1,5 +1,5 @@
 
-#Ejercicio 1A
+        #Ejercicio 1A
 
 class UserAccount:
     def __init__(self,nombre: str, email: str):
@@ -9,9 +9,9 @@ class UserAccount:
         self.followers = []
         self.timeline = []
         self.following = []
-#He elegido para nombre y email cadenas de texto, mientras que para el resto he creado listas. Además he decidido que el nombre sea publico mientras que el email sea privado.
+        #He elegido para nombre y email cadenas de texto, mientras que para el resto he creado listas. Además he decidido que el nombre sea publico mientras que el email sea privado.
 
-#Ejercicio 1B
+        #Ejercicio 1B
     def follow(self,user):
         if not isinstance(user,UserAccount):
             raise Exception("El usuario no es válido")
@@ -20,18 +20,15 @@ class UserAccount:
             user.followers.append(self)
         else:
             print("Ya sigues a :" + user.nombre)
-#En este caso el metodo follow va a recibir datos de tipo UserAccount
+        #En este caso el metodo follow va a recibir datos de tipo UserAccount
 
-#Ejercicio 1C
+        #Ejercicio 1C
     def tweet(self, tweet1: str):
         if len(tweet1)>140:
             raise Exception("Mensaje demasiado largo")
         self.tweets.append(tweet1)
         self.actualizar_tweets_timeline(tweet1)
-
     def actualizar_tweets_timeline(self,tweet2):
-        timeline = self.timeline
         for follower in self.followers:
-            follower(tweet2)
-            timeline.append(follower)
+            follower.timeline.append(tweet2)
             
